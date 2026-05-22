b237164a50943e511a0efcdadc6c322df77e39ab
nq <khangdoannq@gmail.com>
2026-05-22 23:27:51 +0700

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,1 +1,102 @@
+// ==UserScript==
+// @name         MC
+// @namespace    loader.system
+// @version      1.0
+// @description  Private Loader
+// @match        *://*/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
 
+(async () => {
+
+    'use strict';
+
+    // =========================
+    // FILE LINKS
+    // =========================
+
+    const CONFIG =
+    'https://raw.githubusercontent.com/USERNAME/REPO/main/config.json?t=' + Date.now();
+
+    const MAIN =
+    'https://raw.githubusercontent.com/USERNAME/REPO/main/main.js?t=' + Date.now();
+
+    try {
+
+        // =========================
+        // LOAD CONFIG
+        // =========================
+
+        const cfgRes = await fetch(CONFIG, {
+            cache: 'no-store'
+        });
+
+        if (!cfgRes.ok) {
+
+            console.log('[LOADER] CONFIG ERROR');
+
+            return;
+
+        }
+
+        const cfg = await cfgRes.json();
+
+        // =========================
+        // DISABLED
+        // =========================
+
+        if (cfg.st === 'ds') {
+
+            console.log('[LOADER] SCRIPT DISABLED');
+
+            return;
+
+        }
+
+        // =========================
+        // ENABLED
+        // =========================
+
+        if (cfg.st === 'el') {
+
+            console.log('[LOADER] SCRIPT ENABLED');
+
+            const mainRes = await fetch(MAIN, {
+                cache: 'no-store'
+            });
+
+            if (!mainRes.ok) {
+
+                console.log('[LOADER] MAIN ERROR');
+
+                return;
+
+            }
+
+            const code = await mainRes.text();
+
+            // =========================
+            // EXECUTE
+            // =========================
+
+            eval(code);
+
+            return;
+
+        }
+
+        // =========================
+        // INVALID STATUS
+        // =========================
+
+        console.log('[LOADER] INVALID CONFIG');
+
+    } catch (err) {
+
+        console.error('[LOADER ERROR]', err);
+
+    }
+
+})();
